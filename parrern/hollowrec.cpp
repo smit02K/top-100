18948337e4b71ce9341fb1effc9f36d374cb9a78
smit_kalathiya <63186967+smit02K@users.noreply.github.com>
2022-09-17 12:04:14 +0530

    Update hollowrec.cpp

--- parrern/hollowrec.cpp
+++ parrern/hollowrec.cpp
@@ -1,1 +1,21 @@
 
+#include <iostream>
+using namespace std;
+
+// To execute C++, please define "int main()"
+int main() {
+int r=3,c=10;
+
+for(int i=0;i<r;i++){
+  for(int j=0;j<c;j++){
+    if(i==0||j==0||i==r-1||j==c-1){
+       cout<<"* ";
+      }
+    else
+      {cout<<"  ";}
+  }
+  cout<<endl;
+
+}
+
+}
