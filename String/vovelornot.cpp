f07a2e8bb91455b82f2fa81ef2e2520155ef4cd6
smit_kalathiya <63186967+smit02K@users.noreply.github.com>
2022-09-17 11:50:23 +0530

    Update vovelornot.cpp

--- String/vovelornot.cpp
+++ String/vovelornot.cpp
@@ -1,1 +1,16 @@
 
+#include <iostream>
+using namespace std;
+
+// To execute C++, please define "int main()"
+int main() {
+char c;
+cin>>c;
+if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U'){
+  cout<<"vovel";
+}
+else{
+  cout<<"not vove";
+}
+
+}
