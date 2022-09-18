68d7e6834797024dc20ac36fe66e0850d517a48c
smit_kalathiya <63186967+smit02K@users.noreply.github.com>
2022-09-18 22:04:13 +0530

    Create alphabet_or_not.cpp

---
+++ String/alphabet_or_not.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iterator>
+using namespace std;
+
+// To execute C++, please define "int main()"
+int main() {
+char a=' ';// A to Z = 65 to 90 ,a to z = 97 to 122
+   if((a>=65 && a<=90)|| (a>=97 && a<=122)){
+      cout<<"alphabet";
+   }
+   else{cout<<"not alphabet";}
+}
